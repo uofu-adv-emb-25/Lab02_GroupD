4e3803c12154136f1727c87b33b2e2293cada9e3
Emily Yang <u0835353@utah.edu>
2025-09-12 13:17:42 -0600

    Trying to debug tests. Added openocd lines to CMake.

--- test/test.c
+++ test/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
+#include "pico/cyw43_arch.h"
 #include <stdint.h>
 #include <unity.h>
 #include "unity_config.h"
@@ -17,7 +18,6 @@ void tearDown(void)
 
 void test_blinky()
 {
-    setUp();
     int count = 11;
     TEST_ASSERT_TRUE_MESSAGE(blinky(true, count)==true, "Test assert blinky should be off, is on with passed in count 11");
     TEST_ASSERT_TRUE_MESSAGE(blinky(false, count)==false, "Test assert blinky should be on, is off with passed in count 11");
@@ -30,11 +30,11 @@ void test_blinky()
     TEST_ASSERT_TRUE_MESSAGE(blinky(true, count)==false, "Test assert blinky should be on, is off with passed in count 15");
     TEST_ASSERT_TRUE_MESSAGE(blinky(false, count)==true, "Test assert blinky should be off, is on with passed in count 15");
     
-    cyw243_archo_gpio_put(CYW43_WL_GPIO_LED_PIN,0);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,0);
     bool on = 0;
     for(int count = 0; count < 100 ; count++) {
         bool next_on = blinky(on,count);
-        int gpioState = cyw243_archo_gpio_get(CYW43_WL_GPIO_LED_PIN);
+        int gpioState = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
         TEST_ASSERT_EQUAL_MESSAGE(on,gpioState,"GPIO state should be set to value of on");
         if(count % 11) {
             TEST_ASSERT_NOT_EQUAL_MESSAGE(on,next_on,"Should toggle when count is not a multiple of 11");
@@ -43,22 +43,19 @@ void test_blinky()
         }
         on = next_on;
     }
-    tearDown();
 }
 
 void test_changeCase()
 {
-    setUp();
     TEST_ASSERT_TRUE_MESSAGE(changeCase('a')=='A', "Test assert failed, wrong char returned from changecase");
     TEST_ASSERT_TRUE_MESSAGE(changeCase('A')=='a', "Test assert failed, wrong char returned from changecase");
     TEST_ASSERT_TRUE_MESSAGE(changeCase('K')=='k', "Test assert failed, wrong char returned from changecase");
     TEST_ASSERT_TRUE_MESSAGE(changeCase('[')=='[', "Test assert failed, wrong char returned from changecase");
-    tearDown();
 }
 
 void test_variable_assignment()
 {
-    int x = 1;
+    int x = 2;
     TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
 }
 
@@ -76,7 +73,9 @@ int main (void)
         stdio_init_all();
         sleep_ms(5000); // Give time for TTY to attach.
         UNITY_BEGIN();
-        RUN_TEST(test_blinky);
+        RUN_TEST(test_variable_assignment);
+        RUN_TEST(test_multiplication);
+        //RUN_TEST(test_blinky);
         RUN_TEST(test_changeCase);
         sleep_ms(5000);
         UNITY_END();
